5a7d037314da18b5c83a10c909451c3c3b78c508
Shubham Vijay Vargiy <svijay1692@gmail.com>
2020-11-10 20:34:51 +0530

    Add debounce for search app

--- index.js
+++ index.js
@@ -12,74 +12,76 @@ const baseUrl = window.location.host.includes('localhost') ? 'http://localhost:8
 function autocomplete(inp) {
     let currentFocus;
     /*execute a function when someone writes in the text field:*/
-    inp.addEventListener("input", async function (e) {
-        let a, b, i, val = this.value;
-        /*close any already open lists of autocompleted values*/
-        closeAllLists();
-        const selectedElementDiv = document.getElementById('selected-item');
-        selectedElementDiv.style.display = 'none';
-        if (!val) { return false; }
-        const suggestionReq = await fetch(`${baseUrl}/getSuggestion/${val}`);
-        const suggestionRes = await suggestionReq.json();
-        console.log(suggestionRes);
-        const filteredArray = suggestionRes.employees;
-        currentFocus = -1;
-        /*create a DIV element that will contain the items (values):*/
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        /*append the DIV element as a child of the autocomplete container:*/
-        this.parentNode.appendChild(a);
-        const userSectionUI = `
-                <h3><b>{{id}}</b></h3>
-                <h4><b><i>{{name}}</i></b></h4>
-                {{itemSearch}}
-                <p>{{email}}</p>
-            `;
-        filteredArray.forEach((userInformation, index) => {
-            let uiInfo = userSectionUI;
-            if (typeof userInformation.searchByItemsValue === 'number') {
-                uiInfo = uiInfo.replace(new RegExp(`{{itemSearch}}`, "g"), `<ul>
-            <li>
-            <span class="capitalize"> ${
-                userInformation.items[userInformation.searchByItemsValue].replaceAll(val, `<span class="search-key">${val.toLowerCase()}</span>`)
-            } </span> found in items
-            </li>
-        </ul>`);
-            } else {
-                uiInfo = uiInfo.replace(new RegExp(`{{itemSearch}}`, "g"), '');
-            }
-            delete userInformation.searchByItemsValue;
-            for (let key in userInformation) {
-                if (typeof userInformation[key] === 'string') {
-                    uiInfo = uiInfo.replace(new RegExp("{{" + key + "}}", "g"),  userInformation[key].replaceAll(val, `<span class="search-key">${val.toLowerCase()}</span>`));
-                }
-               
-            }
-            uiInfo = uiInfo.replace(new RegExp("{{index}}", "g"), index);
-            uiInfo = uiInfo.replace(/(\{{).+?(\}})/g, " ");
-            // if (val)
-            //     uiInfo = uiInfo.replaceAll(val, `<span class="search-key">${val.toLowerCase()}</span>`);
-
-            /*create a DIV element for each matching element:*/
-            b = document.createElement("DIV");
-            b.innerHTML = uiInfo;
-            /*execute a function when someone clicks on the item value (DIV element):*/
-            b.addEventListener("click", function (e) {
-                console.log(e)
-                /*insert the value for the autocomplete text field:*/
-                const selectedElementDiv = document.getElementById('selected-item');
-                selectedElementDiv.innerHTML = `<h3> Selected Infomation </h3> <br/> ${b.innerHTML}`;
-                selectedElementDiv.style.display = 'block';
-                // selectedElementDiv.innerHTML = e.target.i
-                /*close the list of autocompleted values,
-                (or any other open lists of autocompleted values:*/
-                closeAllLists();
-            });
-            a.appendChild(b);
-        });
-    });
     /*execute a function presses a key on the keyboard:*/
+    async function onSearch (e) {
+            let a, b, i, val = this.value;
+            /*close any already open lists of autocompleted values*/
+            closeAllLists();
+            const selectedElementDiv = document.getElementById('selected-item');
+            selectedElementDiv.style.display = 'none';
+            if (!val) { return false; }
+            const suggestionReq = await fetch(`${baseUrl}/getSuggestion/${val}`);
+            const suggestionRes = await suggestionReq.json();
+            console.log(suggestionRes);
+            const filteredArray = suggestionRes.employees;
+            currentFocus = -1;
+            /*create a DIV element that will contain the items (values):*/
+            a = document.createElement("DIV");
+            a.setAttribute("id", this.id + "autocomplete-list");
+            a.setAttribute("class", "autocomplete-items");
+            /*append the DIV element as a child of the autocomplete container:*/
+            this.parentNode.appendChild(a);
+            const userSectionUI = `
+                    <h3><b>{{id}}</b></h3>
+                    <h4><b><i>{{name}}</i></b></h4>
+                    {{itemSearch}}
+                    <p>{{email}}</p>
+                `;
+            filteredArray.forEach((userInformation, index) => {
+                let uiInfo = userSectionUI;
+                if (typeof userInformation.searchByItemsValue === 'number') {
+                    uiInfo = uiInfo.replace(new RegExp(`{{itemSearch}}`, "g"), `<ul>
+                <li>
+                <span class="capitalize"> ${
+                    userInformation.items[userInformation.searchByItemsValue].replaceAll(val, `<span class="search-key">${val.toLowerCase()}</span>`)
+                } </span> found in items
+                </li>
+            </ul>`);
+                } else {
+                    uiInfo = uiInfo.replace(new RegExp(`{{itemSearch}}`, "g"), '');
+                }
+                delete userInformation.searchByItemsValue;
+                for (let key in userInformation) {
+                    if (typeof userInformation[key] === 'string') {
+                        uiInfo = uiInfo.replace(new RegExp("{{" + key + "}}", "g"),  userInformation[key].replaceAll(val, `<span class="search-key">${val.toLowerCase()}</span>`));
+                    }
+                   
+                }
+                uiInfo = uiInfo.replace(new RegExp("{{index}}", "g"), index);
+                uiInfo = uiInfo.replace(/(\{{).+?(\}})/g, " ");
+                // if (val)
+                //     uiInfo = uiInfo.replaceAll(val, `<span class="search-key">${val.toLowerCase()}</span>`);
+    
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                b.innerHTML = uiInfo;
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function (e) {
+                    console.log(e)
+                    /*insert the value for the autocomplete text field:*/
+                    const selectedElementDiv = document.getElementById('selected-item');
+                    selectedElementDiv.innerHTML = `<h3> Selected Infomation </h3> <br/> ${b.innerHTML}`;
+                    selectedElementDiv.style.display = 'block';
+                    // selectedElementDiv.innerHTML = e.target.i
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            });    
+    }
+    const onSearchDebounce = _.debounce(onSearch, 5000)
+    inp.addEventListener("input", onSearchDebounce)
     inp.addEventListener("keydown", function (e) {
         let x = document.getElementById(this.id + "autocomplete-list");
         if (x) x = x.getElementsByTagName("div");
